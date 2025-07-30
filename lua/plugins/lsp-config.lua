e95821ca223b8f6c4f46a2b1ef276f0b4f0295a8
jorgirerison <jorgipeixoto@alu.ufc.br>
2025-07-30 13:51:49 -0300

    feat:first setup to development for java

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,43 +1,85 @@
 return {
-  { -- for lsp features in code cells / embedded code
-    'jmbuhr/otter.nvim',
-    dev = false,
-    dependencies = {
-      {
-        'neovim/nvim-lspconfig',
-        'nvim-treesitter/nvim-treesitter',
-      },
-    },
-    opts = {},
-  },
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-    end,
-  },
+	{ -- for lsp features in code cells / embedded code
+		"jmbuhr/otter.nvim",
+		dev = false,
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {},
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	-- NOVO: Adicionado para instalar LSPs, depuradores, etc. de forma centralizada
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
+				-- Lista unificada de todas as ferramentas a serem instaladas
+				ensure_installed = {
+					-- Suas ferramentas
+					"lua_ls",
+					"pyright",
+					-- Ferramentas do vídeo
+					"jdtls", --
+					"java-debug-adapter", --
+					"java-test", --
+					"bashls", --
+					"cssls", --
+					"html", --
+					"jsonls", --
+				},
+			})
+			-- Comando para corrigir problema de timing com lazy-loading
+			vim.api.nvim_command("MasonToolsInstall")
+		end,
+	},
+	-- MODIFICADO: Bloco principal de configuração do LSP
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			-- Plugins opcionais, mas úteis, do vídeo
+			{ "j-hui/fidget.nvim", opts = {} }, --
+			{ "folke/neodev.nvim", opts = {} }, --
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			-- Configuração automática de LSPs instalados pelo Mason
+			require("mason-lspconfig").setup({
+				-- Função que roda para cada servidor
+				function(server_name)
+					-- Ignora o 'jdtls' para configuração manual em outro lugar
+					if server_name ~= "jdtls" then
+						lspconfig[server_name].setup({
+							capabilities = capabilities,
+						})
+					end
+				end,
+			})
+
+			-- Configuração específica para o LuaLS para reconhecer o `vim` global
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+							-- workspace = {
+							-- 	library = vim.api.nvim_get_runtime_file("", true),
+							-- },
+							-- runtime = {
+							-- 	version = "LuaJIT",
+							-- },
+						},
+					},
+				},
+			})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,27 +1,32 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-      local null_ls = require("null-ls")
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			local null_ls = require("null-ls")
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.formatting.black,
-          null_ls.builtins.formatting.isort,
-          null_ls.builtins.formatting.rubocop,
-          null_ls.builtins.diagnostics.rubocop,
-          null_ls.builtins.diagnostics.mypy,
-          require("none-ls.diagnostics.eslint"), -- <- substituição correta aqui
-          require("none-ls.diagnostics.flake8"), -- <- substituição correta aqui
-        },
-      })
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.formatting.rubocop,
+					null_ls.builtins.diagnostics.rubocop,
+					null_ls.builtins.diagnostics.mypy,
+					require("none-ls.diagnostics.eslint"), -- <- substituição correta aqui
+					require("none-ls.diagnostics.flake8"), -- <- substituição correta aqui
+				},
+			})
 
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+			vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename, { desc = "Rename" })
+			vim.keymap.set("n", "<leader>gg", vim.lsp.buf.hover, { desc = "" })
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "[G]o to [D]efinition"})
+			vim.keymap.set("n", "<leader>gp", vim.diagnostic.goto_prev, { desc = "[G]o to [P]revious diagnostic" })
+			vim.keymap.set("n", "<leader>gn", vim.diagnostic.goto_next, { desc = "[G]o to [N]ext diagnostic" })
+		end,
+	},
 }
