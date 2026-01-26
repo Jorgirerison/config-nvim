cb3f394b1c1c7d6590f09643204709b2953a716a
jorgirerison <jorgipeixoto@alu.ufc.br>
2026-01-26 14:15:39 -0300

    dap to flask

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -1,175 +1,105 @@
--- Debugging Support
 return {
-  -- https://github.com/rcarriga/nvim-dap-ui
-  'rcarriga/nvim-dap-ui',
-  event = 'VeryLazy',
-  dependencies = {
-    -- https://github.com/mfussenegger/nvim-dap
-    'mfussenegger/nvim-dap',
-    -- https://github.com/nvim-neotest/nvim-nio
-    'nvim-neotest/nvim-nio',
-    -- https://github.com/theHamsta/nvim-dap-virtual-text
-    'theHamsta/nvim-dap-virtual-text', -- inline variable text while debugging
-    -- https://github.com/nvim-telescope/telescope-dap.nvim
-    'nvim-telescope/telescope-dap.nvim', -- telescope integration with dap
-  },
-  opts = {
-    controls = {
-      element = "repl",
-      enabled = false,
-      icons = {
-        disconnect = "",
-        pause = "",
-        play = "",
-        run_last = "",
-        step_back = "",
-        step_into = "",
-        step_out = "",
-        step_over = "",
-        terminate = ""
-      }
-    },
-    element_mappings = {},
-    expand_lines = true,
-    floating = {
-      border = "single",
-      mappings = {
-        close = { "q", "<Esc>" }
-      }
-    },
-    force_buffers = true,
-    icons = {
-      collapsed = "",
-      current_frame = "",
-      expanded = ""
-    },
-    layouts = {
-      {
-        elements = {
-          {
-            id = "scopes",
-            size = 0.50
-          },
-          {
-            id = "stacks",
-            size = 0.30
-          },
-          {
-            id = "watches",
-            size = 0.10
-          },
-          {
-            id = "breakpoints",
-            size = 0.10
-          }
-        },
-        size = 40,
-        position = "left", -- Can be "left" or "right"
-      },
-      {
-        elements = {
-          "repl",
-          "console",
-        },
-        size = 10,
-        position = "bottom", -- Can be "bottom" or "top"
-      }
-    },
-    mappings = {
-      edit = "e",
-      expand = { "<CR>", "<2-LeftMouse>" },
-      open = "o",
-      remove = "d",
-      repl = "r",
-      toggle = "t"
-    },
-    render = {
-      indent = 1,
-      max_value_lines = 100
-    }
-  },
-  config = function (_, opts)
-    local dap = require('dap')
-    require('dapui').setup(opts)
+	"rcarriga/nvim-dap-ui",
+	event = "VeryLazy",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+		"nvim-neotest/nvim-nio",
+		"theHamsta/nvim-dap-virtual-text",
+		"nvim-telescope/telescope-dap.nvim",
+		-- Adicionado para facilitar a integração com Python
+		"mfussenegger/nvim-dap-python",
+	},
+	opts = {
+		-- Mantive seus controles e layouts originais
+		controls = {
+			element = "repl",
+			enabled = false,
+			icons = {
+				disconnect = "",
+				pause = "",
+				play = "",
+				run_last = "",
+				step_back = "",
+				step_into = "",
+				step_out = "",
+				step_over = "",
+				terminate = "",
+			},
+		},
+		layouts = {
+			{
+				elements = {
+					{ id = "scopes", size = 0.50 },
+					{ id = "stacks", size = 0.30 },
+					{ id = "watches", size = 0.10 },
+					{ id = "breakpoints", size = 0.10 },
+				},
+				size = 40,
+				position = "left",
+			},
+			{
+				elements = { "repl", "console" },
+				size = 10,
+				position = "bottom",
+			},
+		},
+		-- ... outros opts que você já tinha
+	},
+	config = function(_, opts)
+		local dap = require("dap")
+		local dapui = require("dapui")
+		dapui.setup(opts)
 
-    -- Customize breakpoint signs
-    vim.api.nvim_set_hl(0, "DapStoppedHl", { fg = "#98BB6C", bg = "#2A2A2A", bold = true })
-    vim.api.nvim_set_hl(0, "DapStoppedLineHl", { bg = "#204028", bold = true })
-    vim.fn.sign_define('DapStopped', { text='', texthl='DapStoppedHl', linehl='DapStoppedLineHl', numhl= '' })
-    vim.fn.sign_define('DapBreakpoint', { text='', texthl='DiagnosticSignError', linehl='', numhl='' })
-    vim.fn.sign_define('DapBreakpointCondition', { text='', texthl='DiagnosticSignWarn', linehl='', numhl='' })
-    vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DiagnosticSignError', linehl='', numhl= '' })
-    vim.fn.sign_define('DapLogPoint', { text='', texthl='DiagnosticSignInfo', linehl='', numhl= '' })
+		-- Estilização dos Breakpoints (Mantida)
+		vim.api.nvim_set_hl(0, "DapStoppedHl", { fg = "#98BB6C", bg = "#2A2A2A", bold = true })
+		vim.api.nvim_set_hl(0, "DapStoppedLineHl", { bg = "#204028", bold = true })
+		vim.fn.sign_define(
+			"DapStopped",
+			{ text = "", texthl = "DapStoppedHl", linehl = "DapStoppedLineHl", numhl = "" }
+		)
+		vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      require('dapui').open()
-    end
+		-- Listeners para abrir o UI automaticamente
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
 
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      -- Commented to prevent DAP UI from closing when unit tests finish
-      -- require('dapui').close()
-    end
+		------------------------------------------------------------------
+		-- CONFIGURAÇÃO PYTHON / DJANGO
+		------------------------------------------------------------------
 
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      -- Commented to prevent DAP UI from closing when unit tests finish
-      -- require('dapui').close()
-    end
+		-- Se você usa venv, o nvim-dap-python tenta encontrar o python automaticamente
+		-- Troque o caminho abaixo pelo caminho do seu interpretador global ou do venv principal
+		require("dap-python").setup("python")
 
-    -- Add dap configurations based on your language/adapter settings
-    -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
-    dap.configurations.java = {
-      {
-        name = "Debug Launch (2GB)";
-        type = 'java';
-        request = 'launch';
-        vmArgs = "" ..
-          "-Xmx2g "
-      },
-      {
-        name = "Debug Attach (8000)";
-        type = 'java';
-        request = 'attach';
-        hostName = "127.0.0.1";
-        port = 8000;
-      },
-      {
-        name = "Debug Attach (5005)";
-        type = 'java';
-        request = 'attach';
-        hostName = "127.0.0.1";
-        port = 5005;
-      },
-      {
-        name = "My Custom Java Run Configuration",
-        type = "java",
-        request = "launch",
-        -- You need to extend the classPath to list your dependencies.
-        -- `nvim-jdtls` would automatically add the `classPaths` property if it is missing
-        -- classPaths = {},
+		dap.configurations.python = {
+			{
+				type = "python",
+				request = "launch",
+				name = "Django: Run Server",
+				program = "${workspaceFolder}/manage.py",
+				args = { "runserver", "--noreload" },
+				django = true,
+				console = "integratedTerminal",
+			},
+			{
+				type = "python",
+				request = "attach",
+				name = "Django: Attach Docker",
+				connect = { port = 5678, host = "127.0.0.1" },
+				pathMappings = {
+					{ localRoot = "${workspaceFolder}", remoteRoot = "/app" },
+				},
+			},
+		}
 
-        -- If using multi-module projects, remove otherwise.
-        -- projectName = "yourProjectName",
-
-        -- javaExec = "java",
-        mainClass = "replace.with.your.fully.qualified.MainClass",
-
-        -- If using the JDK9+ module system, this needs to be extended
-        -- `nvim-jdtls` would automatically populate this property
-        -- modulePaths = {},
-        vmArgs = "" ..
-          "-Xmx2g "
-      },
-    }
-
-    -- keymaps
-    -- Em algum lugar no seu arquivo de keymaps ou de configuração do dap
-    vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "[D]AP: [C]ontinuar" })
-    vim.keymap.set("n", "<leader>dj", dap.step_over, { desc = "[D]AP: Step Over ([j] baixo)" })
-    vim.keymap.set("n", "<leader>dk", dap.step_into, { desc = "[D]AP: Step Into ([k] cima)" })
-    vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "[D]AP: Step [O]ut" })
-    vim.keymap.set("n", "<leader>dX", dap.terminate, { desc = "[D]AP: Terminar/Sair ([X])" })
-    vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "[D]AP: Toggle [B]reakpoint" })
-
-  end
+		-- Keymaps (Mantidos e organizados)
+		vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "[D]AP: [C]ontinuar" })
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "[D]AP: Toggle [B]reakpoint" })
+		vim.keymap.set("n", "<leader>dj", dap.step_over, { desc = "[D]AP: Step Over" })
+		vim.keymap.set("n", "<leader>dk", dap.step_into, { desc = "[D]AP: Step Into" })
+		vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "[D]AP: Step Out" })
+		vim.keymap.set("n", "<leader>dX", dap.terminate, { desc = "[D]AP: Terminar" })
+		vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "[D]AP: [U]I Toggle" })
+	end,
 }
-
